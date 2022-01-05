e3a4173e3210c5d2001f4779ddef66b5ce94a21e
dustin-longenecker <dl-dev@outlook.com>
2022-01-04 18:12:17 -0700

    commit

---
+++ server/src/UserResolver.ts
@@ -0,0 +1,31 @@
+import { hash } from 'bcryptjs';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { User } from './entity/User';
+
+@Resolver()
+export class UserResolver {
+  @Query(() => String)
+  hello() {
+    return 'hi';
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<boolean> {
+    const hashedPassword = await hash(password, 12);
+
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword
+      });
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+
+    return true;
+  }
+  }
---
+++ server/src/entity/User.ts
@@ -0,0 +1,16 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity("users")
+export class User extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column("text")
+    email: string;
+
+    @Column("text")
+    password: string;
+
+}
