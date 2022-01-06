002c0d9e783de5a162a1c26693a4f2f66f207b03
dustin-longenecker <dl-dev@outlook.com>
2022-01-05 19:13:56 -0700

    jsonwebtoken added

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,7 +1,15 @@
-import { hash } from 'bcryptjs';
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { compare, hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { Arg, Mutation, Query, Resolver, ObjectType, Field } from 'type-graphql';
 import { User } from './entity/User';
 
+
+
+@ObjectType() 
+class LoginResponse {
+  @Field()
+  accessToken: string
+}
 @Resolver()
 export class UserResolver {
   @Query(() => String)
@@ -9,6 +17,35 @@ export class UserResolver {
     return 'hi';
   }
 
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+  //access token
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      throw new Error('could not find user');
+    }
+
+    const valid = await compare(password, user.password);
+
+    if(!valid){
+      throw new Error('bad password');
+    }
+
+    //login successful
+    return {
+      accessToken: sign({userId: user.id}, 'secret', {expiresIn: '15m'})
+    }
+  }
+
+
+  //add user
   @Mutation(() => Boolean)
   async register(
     @Arg("email") email: string,
--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,15 +1,18 @@
+import { Field, Int, ObjectType } from "type-graphql";
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-
+@ObjectType()
 @Entity("users")
 export class User extends BaseEntity {
-
+    @Field(() => Int)
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Field(() => String)
     @Column("text")
     email: string;
 
+    @Field(() => String)
     @Column("text")
     password: string;
 
