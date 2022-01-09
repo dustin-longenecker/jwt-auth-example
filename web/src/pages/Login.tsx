f611c15df5b6b819e62400a72a73bb5d5de0ee1d
dustin-longenecker <dl-dev@outlook.com>
2022-01-08 19:48:09 -0700

    added login

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLoginMutation } from '../generated/graphql';
 
 interface Props {
 
 }
 
-export const Login = () => {
-    return <div>Login Page</div>
+export const Login: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [login] = useLoginMutation();
+    const navigate = useNavigate();
+
+    return (
+    <form onSubmit={ async  (e) => {
+        e.preventDefault();
+        console.log('form submitted');
+        console.log(email, password);
+        const response = await  login({
+            variables: { 
+                email,
+                password
+            }
+        })
+        console.log(response);
+        //return to homepage
+        navigate('/');
+    }}>
+        <div>
+            <input 
+            value={email} 
+            placeholder="email" 
+            onChange={ e => {
+                setEmail(e.target.value);
+            }} />
+        </div>
+        <div>
+            <input 
+            type="password"
+            value={password}
+            placeholder="password" 
+            onChange={ e => {
+                setPassword(e.target.value);
+            }} />
+        </div>
+        <button type="submit">Login</button>
+    </form>
+    )
 }
