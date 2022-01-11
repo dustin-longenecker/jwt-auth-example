a3613691a728daca8680dfd476f9642e8277575e
dustin-longenecker <dl-dev@outlook.com>
2022-01-10 19:46:18 -0700

    Check for expired access token using apollo-link-token-refresh and jwt-decode

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -6,7 +6,7 @@ export const createAccessToken = (user: User) => {
     return sign({
         userId: user.id },
         process.env.ACCESS_TOKEN_SECRET!,
-        { expiresIn: '15s'} 
+        { expiresIn: '15m'} 
         );
 }
 export const createRefreshToken = (user: User) => {
